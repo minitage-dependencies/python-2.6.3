7912825f29dbc59c6ad414468a8c0fb8875c2ef7
kiorky <devnull@localhost>
2008-05-27 09:24:21 +0000

    fix ssl

--- hooks/setenv.py
+++ hooks/setenv.py
@@ -20,6 +20,7 @@ _curses_panel _curses_panel.c %(ncurses)s
 readline readline.c %(readline)s
 _socket socketmodule.c
 syslog syslogmodule.c
+_ssl _ssl.c %(ssl)s
 cStringIO cStringIO.c
 cPickle cPickle.c
 pyexpat pyexpat.c -DHAVE_EXPAT_H %(expat)s
@@ -31,7 +32,7 @@ _bsddb _bsddb.c %(db)s
  'readline': '-I%(readline)s/include -L%(readline)s/lib -Wl,-rpath,%(readline)s/lib -lhistory -lreadline' % {
      'readline': os.path.abspath(buildout['readline']['location'])
  },
- 'openssl': '-I%(openssl)s/include -L%(openssl)s/lib -Wl,-rpath,%(openssl)s/lib -lcrypto -lssl' % {
+ 'ssl': '-I%(openssl)s/include -I%(openssl)s/include/openssl -L%(openssl)s/lib -Wl,-rpath -Wl,%(openssl)s/lib -lcrypto -lssl' % {
      'openssl': os.path.abspath(buildout['openssl']['location'])
  },
  'bzip2': '-I%(bzip2)s/include -L%(bzip2)s/lib -Wl,-rpath,%(bzip2)s/lib -lbz2' % {
@@ -40,13 +41,12 @@ _bsddb _bsddb.c %(db)s
  'zlib': '-I%(zlib)s/include -L%(zlib)s/lib -Wl,-rpath,%(zlib)s/lib -lz' % {
      'zlib': os.path.abspath(buildout['zlib']['location'])
  },
- 'ncurses': '-I%(ncurses)s/include/ncurses -I%(ncurses)s/include -L%(ncurses)s/lib -Wl,-rpath,%(ncurses)s/lib -lpanel -lform -lmenu -lncurses' % {
+ 'ncurses': '-I%(ncurses)s/include/ncurses -I%(ncurses)s/include -L%(ncurses)s/lib -Wl,-rpath -Wl,%(ncurses)s/lib -lpanel -lform -lmenu -lncurses' % {
      'ncurses': os.path.abspath(buildout['ncurses']['location'])
  },
  'expat': '-I%(expat)s/include -L%(expat)s/lib -Wl,-rpath,%(expat)s/lib -lexpat ' % {
      'expat': os.path.abspath(buildout['expat']['location'])
  },
- 'openssl': os.path.abspath(buildout['openssl']['location']),
  'crypt': crypt,
 }
 )
